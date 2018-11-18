e8f5c496b692b59a8b992442aa68aeec974aaa75
qinzj <qzjian@gmail.com>
2018-11-18 10:17:02 +0800

    add class has protocol iteration

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -19,38 +19,38 @@ import Darwin
 //print(equalNumbers.allEqual())
 //print(differentNumbers.allEqual())
 
-//protocol HasArea {
-//    var area: Double { get }
-//}
-//class Circle: HasArea {
-//    let pi = 3.1415927
-//    var radius: Double
-//    var area: Double { return pi * radius * radius }
-//    init(radius: Double) { self.radius = radius }
-//}
-//class Country: HasArea {
-//    var area: Double
-//    init(area: Double) { self.area = area }
-//}
+protocol HasArea {
+    var area: Double { get }
+}
+class Circle: HasArea {
+    let pi = 3.1415927
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
 
-//class Animal {
-//    var legs: Int
-//    init(legs: Int) { self.legs = legs }
-//}
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
 
-//let objects: [AnyObject] = [
-//    Circle(radius: 2.0),
-//    Country(area: 243_610),
-//    Animal(legs: 4)
-//]
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
 
-//for object in objects {
-//    if let objectWithArea = object as? HasArea {
-//        print("Area is \(objectWithArea.area)")
-//    } else {
-//        print("Something that doesn't have an area")
-//    }
-//}
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
 
 //@objc protocol CounterDataSource {
 //    @objc optional func increment(forCount count: Int) -> Int
