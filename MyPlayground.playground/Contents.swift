b0d0f50ea1ef359565058a6951149c9842e2c66c
qinzj <qzjian@gmail.com>
2018-11-18 14:22:45 +0800

    add Dice game demo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -254,170 +254,171 @@ class Starship: FullyNamed {
 
 var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
 print(ncc1701.fullName)
-//
-//class SomeClass: NSObject, FirstProtocol, AnotherProtocol {
-//    var mustBeSettable: Int = 1
-//    var doesNotNeedToBeSettable: Int = 2
-//    static var someTypeProperty: Int = 3
-//}
-//
-//protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
-//    // protocol definition goes here
-//}
-//
-//class Dice {
-//    let sides: Int
-//    let generator: RandomNumberGenerator
-//    init(sides: Int, generator: RandomNumberGenerator) {
-//        self.sides = sides
-//        self.generator = generator
-//    }
-//    func roll() -> Int {
-//        return Int(generator.random() * Double(sides)) + 1
-//    }
-//}
-//
-//extension Dice: TextRepresentable {
-//    var textualDescription: String {
-//        return "A \(sides)-sided dice"
-//    }
-//}
-//
-//let d12 = Dice(sides: 12, generator: LinearCongruentialGenerator())
-//print(d12.textualDescription)
-//
-//var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
-//for _ in 1...5 {
-//    print("Random dice roll is \(d6.roll())")
-//}
-//
-//protocol DiceGame {
-//    var dice: Dice { get }
-//    func play()
-//}
-//
-//protocol DiceGameDelegate: AnyObject {
-//    func gameDidStart(_ game: DiceGame)
-//    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
-//    func gameDidEnd(_ game: DiceGame)
-//}
-//
-//class SnakesAndLadders: DiceGame {
-//    let finalSquare = 25
-//    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
-//    var square = 0
-//    var board: [Int]
-//    init() {
-//        board = Array(repeating: 0, count: finalSquare + 1)
-//        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
-//        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
-//    }
-//    weak var delegate: DiceGameDelegate?
-//    func play() {
-//        square = 0
-//        delegate?.gameDidStart(self)
-//        gameLoop: while square != finalSquare {
-//            let diceRoll = dice.roll()
-//            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
-//            switch square + diceRoll {
-//            case finalSquare:
-//                break gameLoop
-//            case let newSquare where newSquare > finalSquare:
-//                continue gameLoop
-//            default:
-//                square += diceRoll
-//                square += board[square]
-//            }
-//        }
-//        delegate?.gameDidEnd(self)
-//    }
-//}
-//
-//extension SnakesAndLadders: TextRepresentable {
-//    var textualDescription: String {
-//        return "A game of Snakes and Ladders with \(finalSquare) squares"
-//    }
-//}
-//
-//protocol PrettyTextRepresentable: TextRepresentable {
-//    var prettyTextualDescription: String { get }
-//}
-//
-//extension PrettyTextRepresentable  {
-//    var prettyTextualDescription: String {
-//        return textualDescription
-//    }
-//}
 
-//extension SnakesAndLadders: PrettyTextRepresentable {
-//    var prettyTextualDescription: String {
-//        var output = textualDescription + ":\n"
-//        for index in 1...finalSquare {
-//            switch board[index] {
-//            case let ladder where ladder > 0:
-//                output += "▲ "
-//            case let snake where snake < 0:
-//                output += "▼ "
-//            default:
-//                output += "○ "
-//            }
-//        }
-//        return output
-//    }
-//}
-//
-//class DiceGameTracker: DiceGameDelegate {
-//    var numberOfTurns = 0
-//    func gameDidStart(_ game: DiceGame) {
-//        numberOfTurns = 0
-//        if game is SnakesAndLadders {
-//            print("Started a new game of Snakes and Ladders")
-//        }
-//        print("The game is using a \(game.dice.sides)-sided dice")
-//    }
-//    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
-//        numberOfTurns += 1
-//        print("Rolled a \(diceRoll)")
-//    }
-//    func gameDidEnd(_ game: DiceGame) {
-//        print("The game lasted for \(numberOfTurns) turns")
-//    }
-//}
-//
-//let tracker = DiceGameTracker()
-//let game = SnakesAndLadders()
-//game.delegate = tracker
-//print(game.textualDescription)
-//print(game.prettyTextualDescription)
-//game.play()
-//
-//extension Array: TextRepresentable where Element: TextRepresentable {
-//    var textualDescription: String {
-//        let itemsAsText = self.map { $0.textualDescription }
-//        return "[" + itemsAsText.joined(separator: ", ") + "]"
-//    }
-//}
-//
-//let myDice = [d6, d12]
-//print(myDice.textualDescription)
-//
-//struct Hamster {
-//    var name: String
-//    var textualDescription: String {
-//        return "A hamster named \(name)"
-//    }
-//}
-//extension Hamster: TextRepresentable {}
-//
-//let simonTheHamster = Hamster(name: "Simon")
-//let somethingTextRepresentable: TextRepresentable = simonTheHamster
-//print(somethingTextRepresentable.textualDescription)
-//print(simonTheHamster.textualDescription)
-//
-//let things: [TextRepresentable] = [game, d12, simonTheHamster]
-//for thing in things {
-//    print(thing.textualDescription)
-//}
+class SomeClass: NSObject, FirstProtocol, AnotherProtocol {
+    var mustBeSettable: Int = 1
+    var doesNotNeedToBeSettable: Int = 2
+    static var someTypeProperty: Int = 3
+}
+
+protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
+    // protocol definition goes here
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+extension Dice: TextRepresentable {
+    var textualDescription: String {
+        return "A \(sides)-sided dice"
+    }
+}
+
+let d12 = Dice(sides: 12, generator: LinearCongruentialGenerator())
+print(d12.textualDescription)
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+
+protocol DiceGameDelegate: AnyObject {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+}
+
+class SnakesAndLadders: DiceGame {
+    let finalSquare = 25
+    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board: [Int]
+    init() {
+        board = Array(repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    weak var delegate: DiceGameDelegate?
+    func play() {
+        square = 0
+        delegate?.gameDidStart(self)
+        gameLoop: while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                continue gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+}
+
+extension SnakesAndLadders: TextRepresentable {
+    var textualDescription: String {
+        return "A game of Snakes and Ladders with \(finalSquare) squares"
+    }
+}
+
+protocol PrettyTextRepresentable: TextRepresentable {
+    var prettyTextualDescription: String { get }
+}
+
+extension PrettyTextRepresentable  {
+    var prettyTextualDescription: String {
+        return textualDescription
+    }
+}
+
+extension SnakesAndLadders: PrettyTextRepresentable {
+    var prettyTextualDescription: String {
+        var output = textualDescription + ":\n"
+        for index in 1...finalSquare {
+            switch board[index] {
+            case let ladder where ladder > 0:
+                output += "▲ "
+            case let snake where snake < 0:
+                output += "▼ "
+            default:
+                output += "○ "
+            }
+        }
+        return output
+    }
+}
+
+class DiceGameTracker: DiceGameDelegate {
+    var numberOfTurns = 0
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakesAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Rolled a \(diceRoll)")
+    }
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+}
+
+let tracker = DiceGameTracker()
+let game = SnakesAndLadders()
+game.delegate = tracker
+print(game.textualDescription)
+print(game.prettyTextualDescription)
+game.play()
+
+extension Array: TextRepresentable where Element: TextRepresentable {
+    var textualDescription: String {
+        let itemsAsText = self.map { $0.textualDescription }
+        return "[" + itemsAsText.joined(separator: ", ") + "]"
+    }
+}
+
+let myDice = [d6, d12]
+print(myDice.textualDescription)
+
+struct Hamster {
+    var name: String
+    var textualDescription: String {
+        return "A hamster named \(name)"
+    }
+}
+
+extension Hamster: TextRepresentable {}
+
+let simonTheHamster = Hamster(name: "Simon")
+let somethingTextRepresentable: TextRepresentable = simonTheHamster
+print(somethingTextRepresentable.textualDescription)
+print(simonTheHamster.textualDescription)
+
+let things: [TextRepresentable] = [game, d12, simonTheHamster]
+for thing in things {
+    print(thing.textualDescription)
+}
 
 //protocol Meh {
 //    func doSomething();
